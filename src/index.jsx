2377be1c79c86a3348d0f119b5abb6c94f90b9fd
Neil Bouiche <neil.bouiche@icloud.com>
2023-09-01 16:27:11 +0200

    click outside tweak

--- src/index.jsx
+++ src/index.jsx
@@ -1,97 +1,55 @@
-/**
- * @fileoverview Modal component for displaying a modal overlay with styled components.
- * @module Modal
- */
 import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 
-/**
- * Modal overlay styled component.
- * @component
- */
-const ModalOverlay = styled.div`
+const ModalWrapper = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.7);
-  z-index: 1000;
-`;
-
-/**
- * @component
- * Modal wrapper styled component.
- */
-const ModalWrapper = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #dde7f1;
-  padding: 50px;
-  z-index: 1000;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
   display: flex;
-  flex-direction: column;
-  gap: 30px;
-  border-radius: 20px;
-  border: 10px solid #334662;
+  justify-content: center;
+  align-items: center;
 `;
 
-/**
- * Close button styled component.
- * @component
- */
-const CloseButton = styled.button`
-  display: inline-block;
-  border: none;
+const ModalContent = styled.div`
+  background: white;
+  padding: 20px;
   border-radius: 5px;
-  padding: 1rem 2rem;
-  margin: 0;
-  text-decoration: none;
-  background: #334662;
-  color: #ffffff;
-  font-family: sans-serif;
-  font-size: 1rem;
-  font-weight: 600;
-  line-height: 1;
-  cursor: pointer;
-  text-align: center;
-  transition: background 250ms ease-in-out, transform 150ms ease;
-  -webkit-appearance: none;
-  -moz-appearance: none;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
 `;
 
-/**
- * Modal component for displaying a modal overlay.
- *
- * @param {Object} props - The component props.
- * @param {boolean} props.isOpen - Flag indicating whether the modal is open or not.
- * @param {Function} props.onClose - Callback function to close the modal.
- * @param {ReactNode} props.children - The content to be displayed inside the modal.
- *
- * @returns {JSX.Element|null} The modal component.
- *
- * @example
- * <Modal isOpen={isOpen} onClose={handleClose}>
- *   <p>Modal content</p>
- * </Modal>
- */
-export const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) {
-    return null;
-  }
+const Modal = ({ isOpen, onClose, children }) => {
+  const modalRef = useRef(null);
 
-  const handleWrapperClick = (event) => {
-    event.stopPropagation();
+  const handleClickOutside = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      onClose();
+    }
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
   return (
-    <ModalOverlay onClick={onClose}>
-      <ModalWrapper onClick={handleWrapperClick}>
+    <ModalWrapper>
+      <ModalContent ref={modalRef}>
         {children}
-        <CloseButton onClick={onClose}>Close</CloseButton>
-      </ModalWrapper>
-    </ModalOverlay>
+        <button onClick={onClose}>Close</button>
+      </ModalContent>
+    </ModalWrapper>
   );
 };
+
+export default Modal;
